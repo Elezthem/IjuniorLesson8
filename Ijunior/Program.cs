63afe1973f3e532a902aaec73683c8607025bb0e
HemyDev <97851451+Elezthem@users.noreply.github.com>
2023-08-25 23:39:17 +0300

    Add files via upload

---
+++ Ijunior/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите своё имя: ");
+string name = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write("Введите свой возраст: ");
+string age = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write($"Благодарим, {name} ({age}), за введенную информацию! Очень важный спам будет отправляться вам на почту!");
